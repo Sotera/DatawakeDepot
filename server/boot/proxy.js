83d549abef2b208e6003d7cf202e55e42d3667bd
jreeme <user@nowhere.com>
2015-10-05 15:45:07 -0500

    Start in in proxy-servering.

---
+++ server/boot/proxy.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function(app) {
+  //var httpProxy = require('http-proxy');
+  //httpProxy.createProxyServer({}).listen(8484);
+  //httpProxy.createProxyServer({target: 'http://localhost:3000'}).listen(8484);
+};

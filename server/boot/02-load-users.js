43a96f5519e0f1ccad3e5d2dfcc2dfd863c4a034
mframe <michael.frame@soteradefense.com>
2015-10-14 18:15:39 -0500

    update data models with proper ids and keys, working teamUsers

--- server/boot/02-load-users.js
+++ server/boot/02-load-users.js
@@ -5,9 +5,9 @@ var log = require('debug')('boot:02-load-users');
 
 module.exports = function(app) {
 
-  if (app.dataSources.db.name !== 'Memory' && !process.env.INITDB) {
-    return;
-  }
+  /*  if (app.dataSources.db.name !== 'Memory' && !process.env.INITDB) {
+   return;
+   }*/
 
   createDefaultUsers();
 
@@ -39,36 +39,36 @@ module.exports = function(app) {
 
     roles.forEach(function(role) {
       Role.findOrCreate(
-        {where: {name: role.name}}, // find
-        {name: role.name}, // create
-        function(err, createdRole, created) {
-          if (err) {
-            console.error('error running findOrCreate('+role.name+')', err);
-          }
-          (created) ? log('created role', createdRole.name)
-                    : log('found role', createdRole.name);
-          role.users.forEach(function(roleUser) {
-            AminoUser.findOrCreate(
-              {where: {username: roleUser.username}}, // find
-              roleUser, // create
-              function(err, createdUser, created) {
-                if (err) {
-                  console.error('error creating roleUser', err);
-                }
-                (created) ? log('created user', createdUser.username)
-                          : log('found user', createdUser.username);
-                createdRole.principals.create({
-                  principalType: RoleMapping.USER,
-                  principalId: createdUser.id
-                }, function(err, rolePrincipal) {
-                  if (err) {
-                    console.error('error creating rolePrincipal', err);
-                  }
-                  users.push(createdUser);
-                });
-              });
+          {where: {name: role.name}}, // find
+          {name: role.name}, // create
+          function(err, createdRole, created) {
+            if (err) {
+              console.error('error running findOrCreate('+role.name+')', err);
+            }
+            (created) ? log('created role', createdRole.name)
+                : log('found role', createdRole.name);
+            role.users.forEach(function(roleUser) {
+              AminoUser.findOrCreate(
+                  {where: {username: roleUser.username}}, // find
+                  roleUser, // create
+                  function(err, createdUser, created) {
+                    if (err) {
+                      console.error('error creating roleUser', err);
+                    }
+                    (created) ? log('created user', createdUser.username)
+                        : log('found user', createdUser.username);
+                    createdRole.principals.create({
+                      principalType: RoleMapping.USER,
+                      principalId: createdUser.id
+                    }, function(err, rolePrincipal) {
+                      if (err) {
+                        console.error('error creating rolePrincipal', err);
+                      }
+                      users.push(createdUser);
+                    });
+                  });
+            });
           });
-        });
     });
     return users;
   }

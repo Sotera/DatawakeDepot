adfda4fcc5ba9ad9e830494f57576ad666b47e16
mframe <michael.frame@soteradefense.com>
2015-10-14 10:32:03 -0500

    fix search on domain items

--- client/app/modules/dwDomains/controllers/domains.ctrl.js
+++ client/app/modules/dwDomains/controllers/domains.ctrl.js
@@ -1,12 +1,11 @@
 'use strict';
 var app = angular.module('com.module.dwDomains');
 
-app.controller('DomainsCtrl', function($scope, $state, $stateParams, DomainsService, gettextCatalog, AppAuth) {
+app.controller('DomainsCtrl', function($scope, $state, $stateParams, DwDomain, DomainsService, gettextCatalog, AppAuth) {
 
     //Put the currentUser in $scope for convenience
     $scope.currentUser = AppAuth.currentUser;
 
-    $scope.domain = {};
     $scope.formFields = [{
         key: 'name',
         type: 'input',
@@ -26,26 +25,95 @@ app.controller('DomainsCtrl', function($scope, $state, $stateParams, DomainsServ
 
     $scope.delete = function(id) {
         DomainsService.deleteDomain(id, function() {
-            $scope.domains = DomainsService.getDomains();
+            $scope.safeDisplayedDomains = DomainsService.getDomains();
             $state.go('^.list');
         });
     };
 
     $scope.onSubmit = function() {
-        DomainsService.upsertDomain($scope.domain, function() {
-            $scope.domains = DomainsService.getDomains();
+        DomainsService.upsertDomain($scope.safeDisplayedDomains, function() {
+            $scope.safeDisplayedDomains = DomainsService.getDomains();
             $state.go('^.list');
         });
     };
 
-    $scope.domains = DomainsService.getDomains();
+    $scope.loading = true;
+    DwDomain.find({filter: {include: []}}).$promise
+        .then(function (allDomains) {
+            $scope.safeDisplayedDomains = allDomains;
+            $scope.displayedDomains = [].concat($scope.safeDisplayedUsers);
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+        .then(function () {
+            $scope.loading = false;
+        });
+    return;
 
     if ($stateParams.id) {
         DomainsService.getDomain($stateParams.id).$promise.then(function(result){
-            $scope.domain = result;})
+            $scope.safeDisplayedDomains = result;})
     } else {
-        $scope.domain = {};
+        $scope.safeDisplayedDomains = {};
     }
 
+    //Search Functionality
+    function arrayObjectIndexOf(myArray, searchTerm, property) {
+        for (var i = 0, len = myArray.length; i < len; i++) {
+            if (myArray[i][property] === searchTerm) return i;
+        }
+        return -1;
+    }
+    $scope.aToB = function () {
+        for (var i in $scope.selectedA) {
+            var moveId = arrayObjectIndexOf($scope.items, $scope.selectedA[i], "id");
+            $scope.listB.push($scope.items[moveId]);
+            var delId = arrayObjectIndexOf($scope.listA, $scope.selectedA[i], "id");
+            $scope.listA.splice(delId, 1);
+        }
+        reset();
+    };
+    $scope.bToA = function () {
+        for (var i in $scope.selectedB) {
+            var moveId = arrayObjectIndexOf($scope.items, $scope.selectedB[i], "id");
+            $scope.listA.push($scope.items[moveId]);
+            var delId = arrayObjectIndexOf($scope.listB, $scope.selectedB[i], "id");
+            $scope.listB.splice(delId, 1);
+        }
+        reset();
+    };
+    function reset() {
+        $scope.selectedA = [];
+        $scope.selectedB = [];
+        $scope.toggle = 0;
+    }
+
+    $scope.toggleA = function () {
+        if ($scope.selectedA.length > 0) {
+            $scope.selectedA = [];
+        }
+        else {
+            for (var i in $scope.listA) {
+                $scope.selectedA.push($scope.listA[i].id);
+            }
+        }
+    }
+    $scope.toggleB = function () {
+        if ($scope.selectedB.length > 0) {
+            $scope.selectedB = [];
+        }
+        else {
+            for (var i in $scope.listB) {
+                $scope.selectedB.push($scope.listB[i].id);
+            }
+        }
+    }
+    $scope.selectA = function (i) {
+        $scope.selectedA.push(i);
+    };
+    $scope.selectB = function (i) {
+        $scope.selectedB.push(i);
+    };
 });
 

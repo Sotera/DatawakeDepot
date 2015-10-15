1b5a4bdc1aae5e096ef83c4411c564828eb5586e
jreeme <user@nowhere.com>
2015-10-15 07:35:12 -0500

    Commit proxy so I can merge in master

--- server/boot/02-load-users.js
+++ server/boot/02-load-users.js
@@ -1,76 +1,73 @@
 'use strict';
-
 // to enable these logs set `DEBUG=boot:02-load-users` or `DEBUG=boot:*`
+var async = require('async');
 var log = require('debug')('boot:02-load-users');
-
-module.exports = function(app) {
-
-/*  if (app.dataSources.db.name !== 'Memory' && !process.env.INITDB) {
-    return;
-  }*/
-
-  createDefaultUsers();
-
-  function createDefaultUsers() {
-
-    log('Creating roles and users');
-
-    var AminoUser = app.models.AminoUser;
-    var Role = app.models.Role;
-    var RoleMapping = app.models.RoleMapping;
-
-    var users = [];
-    var roles = [{
-      name: 'admins',
-      users: [{
-        firstName: 'Admin',
-        lastName: 'User',
-        email: 'admin@admin.com',
-        username: 'admin',
-        password: 'admin'
-      }]
-    }, {
-      name: 'guests',
-      users: []
-    }, {
-      name: 'users',
-      users: []
-    }];
-
-    roles.forEach(function(role) {
-      Role.findOrCreate(
-        {where: {name: role.name}}, // find
-        {name: role.name}, // create
-        function(err, createdRole, created) {
+module.exports = function (app) {
+  var AminoUser = app.models.AminoUser;
+  var Role = app.models.Role;
+  var DwTeam = app.models.DwTeam;
+  var RoleMapping = app.models.RoleMapping;
+  var adminUser = {
+    firstName: 'Admin',
+    lastName: 'User',
+    email: 'admin@admin.com',
+    username: 'admin',
+    password: 'admin'
+  };
+  var johnny = {
+    firstName: 'John',
+    lastName: 'Reeme',
+    email: 'jreeme@gmail.com',
+    username: 'jreeme',
+    password: 'password'
+  };
+  async.parallel([
+    createDefaultUserInfo
+  ], function (err, result) {
+    if (err) {
+      log(err);
+      return;
+    }
+    //Add admin to admins group
+    Role.findOne({where: {name: 'admins'}}, function (err, role) {
+      AminoUser.findOne({where: {username: 'admin'}}, function (err, user) {
+        role.principals.create({
+          principalType: RoleMapping.USER,
+          principalId: user.id
+        }, function (err, roleMapping) {
           if (err) {
-            console.error('error running findOrCreate('+role.name+')', err);
+            log('error creating rolePrincipal', err);
           }
-          (created) ? log('created role', createdRole.name)
-                    : log('found role', createdRole.name);
-          role.users.forEach(function(roleUser) {
-            AminoUser.findOrCreate(
-              {where: {username: roleUser.username}}, // find
-              roleUser, // create
-              function(err, createdUser, created) {
-                if (err) {
-                  console.error('error creating roleUser', err);
-                }
-                (created) ? log('created user', createdUser.username)
-                          : log('found user', createdUser.username);
-                createdRole.principals.create({
-                  principalType: RoleMapping.USER,
-                  principalId: createdUser.id
-                }, function(err, rolePrincipal) {
-                  if (err) {
-                    console.error('error creating rolePrincipal', err);
-                  }
-                  users.push(createdUser);
-                });
-              });
-          });
         });
+      });
     });
-    return users;
+  });
+  function createDefaultUserInfo(cb) {
+    var roles = [{name: 'admins'}, {name: 'users'}, {name: 'guests'}];
+    var teams = [{name: 'Team A'}, {name: 'Team B'}, {name: 'Team C'}];
+    var users = [adminUser, johnny];
+    var functionArray = [];
+    roles.forEach(function (role) {
+      functionArray.push(async.apply(findOrCreateObj, Role, {where: {name: role.name}}, role));
+    });
+    teams.forEach(function (team) {
+      functionArray.push(async.apply(findOrCreateObj, DwTeam, {where: {name: team.name}}, team));
+    });
+    users.forEach(function (user) {
+      functionArray.push(async.apply(findOrCreateObj, AminoUser, {where: {username: user.username}}, user));
+    });
+    async.parallel(functionArray, cb);
   }
 
+  function findOrCreateObj(model, query, objToCreate, cb) {
+    model.findOrCreate(
+      query,
+      objToCreate, // create
+      function (err, createdObj) {
+        if(err){
+          log(err);
+        }
+        cb(err, createdObj);
+      });
+  }
 };

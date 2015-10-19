005456e371016aec9e3f8687ca3e85e6f9454e29
bmcdougald <brodie.mcdougald@soteradefense.com>
2015-10-19 14:18:11 -0500

    Icon changes for Dashboard items

--- client/app/modules/dwServiceTypes/config/serviceTypes.config.js
+++ client/app/modules/dwServiceTypes/config/serviceTypes.config.js
@@ -2,10 +2,10 @@
 var app = angular.module('com.module.dwServiceTypes');
 
 app.run(function($rootScope, DwServiceType, gettextCatalog) {
-    $rootScope.addMenu(gettextCatalog.getString('ServiceTypes'), 'app.dwServiceTypes.list', 'ion-ios-pulse-strong');
+    $rootScope.addMenu(gettextCatalog.getString('ServiceTypes'), 'app.dwServiceTypes.list', 'fa-sellsy');
 
-    DwServiceType.find(function(data) {
-        $rootScope.addDashboardBox(gettextCatalog.getString('ServiceTypes'), 'bg-blue', 'ion-ios-pulse-strong', data.length, 'app.dwServiceTypes.list');
-    });
+    //DwServiceType.find(function(data) {
+    //    $rootScope.addDashboardBox(gettextCatalog.getString('ServiceTypes'), 'bg-blue', 'fa-sellsy', data.length, 'app.dwServiceTypes.list');
+    //});
 
 });
--- client/app/modules/dwUrlExtractions/config/urlExtractions.config.js
+++ client/app/modules/dwUrlExtractions/config/urlExtractions.config.js
@@ -2,10 +2,10 @@
 var app = angular.module('com.module.dwUrlExtractions');
 
 app.run(function($rootScope, DwUrlExtraction, gettextCatalog) {
-    $rootScope.addMenu(gettextCatalog.getString('UrlExtractions'), 'app.dwUrlExtractions.list', 'fa-share-square-o');
+    $rootScope.addMenu(gettextCatalog.getString('UrlExtractions'), 'app.dwUrlExtractions.list', 'fa-newspaper-o');
 
-    DwUrlExtraction.find(function(data) {
-        $rootScope.addDashboardBox(gettextCatalog.getString('UrlExtractions'), 'bg-blue', 'ion-share', data.length, 'app.dwUrlExtractions.list');
-    });
+    //DwUrlExtraction.find(function(data) {
+    //    $rootScope.addDashboardBox(gettextCatalog.getString('UrlExtractions'), 'bg-blue', 'ion-share', data.length, 'app.dwUrlExtractions.list');
+    //});
 
 });

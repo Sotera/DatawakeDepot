a88a4f3455e752a9f0b87ec2ff82d3f7e82afbe8
jreeme <brodie.mcdougald@soteradefense.com>
2015-10-19 13:04:21 -0500

    Changed order of sidebar menu items and adjusted teams placard

--- client/app/modules/dwTeams/config/teams.config.js
+++ client/app/modules/dwTeams/config/teams.config.js
@@ -5,7 +5,7 @@ app.run(function($rootScope, DwTeam, gettextCatalog) {
   $rootScope.addMenu(gettextCatalog.getString('Teams'), 'app.dwTeams.list', 'fa-group');
 
   DwTeam.find(function(data) {
-    $rootScope.addDashboardBox(gettextCatalog.getString('Teams'), 'bg-teal', 'ion-clipboard', data.length, 'app.dwTeams.list');
+    $rootScope.addDashboardBox(gettextCatalog.getString('Teams'), 'bg-blue', 'ion-ios-people', data.length, 'app.dwTeams.list');
   });
 
 });

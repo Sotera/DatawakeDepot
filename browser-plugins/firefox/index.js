00221ed8731d91bb344ec3ce95145dda893901ec
user nowhere <user@nowhere.com>
2015-10-27 08:11:45 -0500

    Changed URL in plugin to point to datawake-depot.org

--- browser-plugins/firefox/index.js
+++ browser-plugins/firefox/index.js
@@ -1,8 +1,8 @@
 var {pluginState} = require('./data/pluginState');
-pluginState.pageModDatawakeDepotIncludeFilter = 'http://localhost:3000/*';
-//pluginState.pageModDatawakeDepotIncludeFilter = '*.datawake-depot.org';
-pluginState.loginUrl = 'http://localhost:3000';
-//pluginState.loginUrl = 'http://datawake-depot.org';
+//pluginState.pageModDatawakeDepotIncludeFilter = 'http://localhost:3000/*';
+pluginState.pageModDatawakeDepotIncludeFilter = '*.datawake-depot.org';
+//pluginState.loginUrl = 'http://localhost:3000';
+pluginState.loginUrl = 'http://datawake-depot.org';
 require('./data/contextMenu').init();
 require('./data/pageMods').init();
 require('./data/addin').init();

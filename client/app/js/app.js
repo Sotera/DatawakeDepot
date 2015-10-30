325099323b0bc9696baa1234c0becaf951249c16
BTWhiteman <bwhiteman@gmail.com>
2015-10-30 12:06:20 -0400

    Adding multiselect dropdown

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -14,6 +14,7 @@ angular.module('loopbackApp', [
   'angular.filter',
   'angularBootstrapNavTree',
   'angularFileUpload',
+  'angularjs-dropdown-multiselect',
   'btford.markdown',
   'oitozero.ngSweetAlert',
   'config',

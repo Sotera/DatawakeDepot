7891e8b182c81f2e3c85b421a89aab59d8bfd205
bmcdougald <brodie.mcdougald@soteradefense.com>
2016-01-21 13:02:44 -0600

    fixed existing trail message

--- browser-plugins/firefox/data/toolbarFrame.js
+++ browser-plugins/firefox/data/toolbarFrame.js
@@ -323,6 +323,7 @@ function cancelTrail(){
     window.createTrailMode = false;
     window.refreshTrails = false;
     $('#trailInput').val(window.addTrailText);
+    $('#divError').hide();
 }
 
 function openTab(tabTarget){

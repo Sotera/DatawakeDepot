e5db60df559caaf79f33344fd4b557558d876aa6
Michael Frame <michael.frame@soteradefense.com>
2016-01-21 17:36:45 -0600

    set $scope.currentUser to Amino for header, send logout to plugin outside of AppAuth logout

--- client/app/modules/core/controllers/main.ctrl.js
+++ client/app/modules/core/controllers/main.ctrl.js
@@ -12,16 +12,17 @@
  **/
 angular.module('com.module.core')
   .controller('MainCtrl', function ($scope, $rootScope, $state, $location, UserLoginOrLogoutMsg,
-                                    CoreService, AppAuth, gettextCatalog) {
+                                    CoreService, AppAuth, gettextCatalog, AminoUser) {
     //This currentUser is for filling out the Login screen and has nothing to do with
     //whether anyone is logged in
     UserLoginOrLogoutMsg.listen(function (_event, msg) {
       if(msg.action === 'initiate-logout'){
+        UserLoginOrLogoutMsg.broadcast({action: 'logout'});
         AppAuth.logout(function () {
           $state.go('login');
           CoreService.toastSuccess(gettextCatalog.getString('Logged out'), gettextCatalog.getString('You are logged out!'));
           //Notify toolbar
-          UserLoginOrLogoutMsg.broadcast({action: 'logout'});
+
         });
       }else if(msg.action === 'logout'){
         $scope.noOneLoggedIn = true;
@@ -30,7 +31,8 @@ angular.module('com.module.core')
       }
     });
 
-    $scope.currentUser = AppAuth.getCurrentUser();
+    AppAuth.getCurrentUser();
+    $scope.currentUser = AminoUser.getCurrent();
     $scope.menuoptions = $rootScope.menu;
 
     $scope.logout = function () {

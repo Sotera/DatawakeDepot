cf4a168c96744f828c23c896104d76c0c2a1014c
Michael Frame <michael.frame@soteradefense.com>
2016-02-08 12:59:28 -0600

    alter to use env vars if they exist, otherwise run local

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 'use strict';
-/*
- var mongoAddr = '"' + process.env.MONGO_PORT_27017_TCP_ADDR + '"';
- var mongoPort = process.env.MONGO_PORT_27017_TCP_PORT;
- var fs = require('fs');
 
- var fileContents = fs.readFileSync(__dirname + '/datasources_template.json', 'utf8');
- fileContents = fileContents.replace(/MONGO_HOST_ADDR/g, mongoAddr);
- fileContents = fileContents.replace(/MONGO_HOST_PORT/g, mongoPort);
- fs.writeFileSync(__dirname + '/datasources.json', fileContents);
- */
+var mongoAddr = process.env.MONGO_PORT_27017_TCP_ADDR||"localhost";
+var mongoPort = process.env.MONGO_PORT_27017_TCP_PORT||"27017";
+var fs = require('fs');
+
+var fileContents = fs.readFileSync(__dirname + '/datasources_template.json', 'utf8');
+fileContents = fileContents.replace(/MONGO_HOST_ADDR/g, mongoAddr);
+fileContents = fileContents.replace(/MONGO_HOST_PORT/g, mongoPort);
+fs.writeFileSync(__dirname + '/datasources.json', fileContents);
+
 var loopback = require('loopback');
 var boot = require('loopback-boot');
 var path = require('path');

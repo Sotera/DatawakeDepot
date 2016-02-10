8211ece7c6d7a71c0010dcff16bbe98a04e7d217
bmcdougald <brodie.mcdougald@soteradefense.com>
2016-02-10 13:23:58 -0600

    added relative path to jpm after the changes to package.json so executable can be found to build XPI

--- browser-plugins/firefox/_runFF.sh
+++ browser-plugins/firefox/_runFF.sh
@@ -2,7 +2,7 @@
 #jpm run -b /usr/bin/firefox -p datawake
 
 #Uses Extension Auto-Installer plugin (v.1.2.2.1-signed) by Wladimir Palant
-jpm xpi
+../../node_modules/jpm/bin/jpm xpi
 mkdir ../../client/app/xpi
 cp @datawakeffplugin-0.0.2.xpi ../../client/app/xpi/datawakePlugin.xpi
 wget --post-file=@datawakeffplugin-0.0.2.xpi http://localhost:8889/

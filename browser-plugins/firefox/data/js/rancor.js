394f2eb454cb83eea09edc3ab4c403c59b5ec1e6
Michael Frame <michael.frame@soteradefense.com>
2016-02-10 15:13:37 -0600

    rancor click opens new tab

--- browser-plugins/firefox/data/js/rancor.js
+++ browser-plugins/firefox/data/js/rancor.js
@@ -33,13 +33,8 @@ function drawRancor(urlResults) {
     };
     network = new vis.Network(document.getElementById('widgetTwo'), data, options);
 
-        //network.on("showPopup", function (params) {
-        //    document.getElementById('eventSpan').innerHTML = '<h2>showPopup event: </h2>' + JSON.stringify(params, null, 4);
-        //});
-        //// add event listeners
-        //network.on('select', function (params) {
-        //    //document.getElementById('selection').innerHTML = 'Selection: ' + params.nodes;
-        //    document.getElementById('eventSpan').innerHTML = '<h2>Selected Node: </h2> <a href="' + data.nodes[params.nodes[0]].url + '">' +
-        //        data.nodes[params.nodes[0]].url + '</a>';
-        //});
-    }
+    // add event listeners
+    network.on('select', function (params) {
+        window.open(data.nodes[params.nodes[0]].url, '_blank');
+    });
+}

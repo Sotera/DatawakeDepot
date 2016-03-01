0e4bef614fa24689a02bc3b1e1d23463a2b1084b
Michael Frame <michael.frame@soteradefense.com>
2016-03-01 13:59:04 -0600

    merged latest dev and sug fixes

--- browser-plugins/firefox/index.js
+++ browser-plugins/firefox/index.js
@@ -6,7 +6,7 @@ var {pluginState} = require('./data/pluginState');
 pluginState.pageModDatawakeDepotIncludeFilter = 'http://localhost:3000/*';
 pluginState.loginUrl = 'http://localhost:3000';
 //Edit this to be a valid Rancor instance
-pluginState.trailUrlRancor = 'http://localhost:3004/api/rank/process';
+pluginState.trailUrlRancor = 'http://localhost:3000/api/rank/process';
 
 require('./data/contextMenu').init();
 require('./data/pageMods').init();

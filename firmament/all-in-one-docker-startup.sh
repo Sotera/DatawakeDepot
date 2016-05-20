47325ae1c0e5b5343274cd212ad72ebac554a71d
John Reeme <john.reeme@soteradefense.com>
2016-05-20 12:04:41 -0500

    Change 19 to 20

--- firmament/all-in-one-docker-startup.sh
+++ firmament/all-in-one-docker-startup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -dt --name datawake_all -h datawake -p 8182:8182 -p 8701:8701 -p 8082:3001 jreeme/datawake:19-MAY-2016
+docker run -dt --name datawake_all -h datawake -p 8182:8182 -p 8701:8701 -p 8082:3001 jreeme/datawake:20-MAY-2016

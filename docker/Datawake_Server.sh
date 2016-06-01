7c15ca76abd1a466dddc57467b2c908650cf3b84
Michael Frame <Michael.Frame@SoteraDefense.com>
2016-06-01 12:59:02 -0500

    Update Datawake_Server.sh

--- docker/Datawake_Server.sh
+++ docker/Datawake_Server.sh
@@ -11,4 +11,4 @@ docker rm datawake
 #comment out next line to persist data between restarts
 docker run -dt --name dw-data -h dw-data sotera/data-container
 docker run -dt --volumes-from dw-data --name dw-mongo -h mongo sotera/datawake-mongo
-docker run -dt --name datawake -h datawake --link dw-mongo:mongo --link -p 8701:8701 -p 3001:3001 sotera/datawake-strongloop
+docker run -dt --name datawake -h datawake --link dw-mongo:mongo -p 8701:8701 -p 3001:3001 sotera/datawake-strongloop

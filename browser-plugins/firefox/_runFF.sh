acd2a7f7a8dc6b90ed4e292720381a798bb45441
John Reeme <john.reeme@soteradefense.com>
2017-03-06 15:16:11 -0600

    Init Comit!

--- browser-plugins/firefox/_runFF.sh
+++ browser-plugins/firefox/_runFF.sh
@@ -3,6 +3,6 @@
 
 #Uses Extension Auto-Installer plugin (v.1.2.2.1-signed) by Wladimir Palant
 ../../node_modules/jpm/bin/jpm xpi
-mkdir ../../client/app/xpi
-cp @datawakeffplugin-0.0.2.xpi ../../client/app/xpi/datawakePlugin.xpi
-
+mkdir -p ../../client/app/xpi
+cp datawakeffplugin.xpi ../../client/app/xpi/datawakePlugin.xpi
+wget --post-file=datawakeffplugin.xpi http://localhost:8888/

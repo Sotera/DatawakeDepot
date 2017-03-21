fc88d95577a692f04510114c8846e16c6a217562
jreeme <john.reeme@soteradefense.com>
2017-03-21 15:40:18 -0500

    Change port on plugin for use in docker container

--- client/app/modules/dwDomainItems/controllers/domainItems.ctrl.js
+++ client/app/modules/dwDomainItems/controllers/domainItems.ctrl.js
@@ -2,217 +2,226 @@
 var app = angular.module('com.module.dwDomainItems');
 
 app.controller('DomainItemsCtrl', function ($scope, $state, $stateParams, AminoUser, DwDomain, DomainItemsService, gettextCatalog, AppAuth) {
-    $scope.domains = [];
-    $scope.plUsers = [];
-    $scope.currentDomainId = '';
-    $scope.domainItem = {};
+  $scope.domains = [];
+  $scope.plUsers = [];
+  $scope.currentDomainId = '';
+  $scope.domainItem = {};
 
-    $scope.itemIndex = 0;
-    $scope.itemsPerPage = 15;
+  $scope.itemIndex = 0;
+  $scope.itemsPerPage = 15;
 
-    $scope.formFields = [{
-        key: 'id',
-        type: 'input',
-        templateOptions: {
-            label: gettextCatalog.getString('id'),
-            disabled: true
-        }
-    }, {
-        key: 'dwDomainId',
-        type: 'select',
-        templateOptions: {
-            label: gettextCatalog.getString('Domain'),
-            options: $scope.domains,
-            valueProp: 'id',
-            labelProp: 'name',
-            required: true
-        }
-        ,
-        expressionProperties: {
-            'templateOptions.disabled': 'model.id'
-        }
-    }, {
-        key: 'itemValue',
-        type: 'input',
-        templateOptions: {
-            label: gettextCatalog.getString('Value'),
-            required: true
-        }
-    }, {
-        key: 'userId',
-        type: 'select',
-        templateOptions: {
-            label: gettextCatalog.getString('User'),
-            options: $scope.plUsers,
-            valueProp: 'id',
-            labelProp: 'name',
-            required: false,
-            disabled: true
-        }
-    }, {
-        key: 'coreItem',
-        type: 'checkbox',
-        templateOptions: {
-            label: gettextCatalog.getString('Core Item?'),
-            required: false
-        }
-    }];
+  $scope.formFields = [{
+    key: 'id',
+    type: 'input',
+    templateOptions: {
+      label: gettextCatalog.getString('id'),
+      disabled: true
+    }
+  }, {
+    key: 'dwDomainId',
+    type: 'select',
+    templateOptions: {
+      label: gettextCatalog.getString('Domain'),
+      options: $scope.domains,
+      valueProp: 'id',
+      labelProp: 'name',
+      required: true
+    }
+    ,
+    expressionProperties: {
+      'templateOptions.disabled': 'model.id'
+    }
+  }, {
+    key: 'itemValue',
+    type: 'input',
+    templateOptions: {
+      label: gettextCatalog.getString('Value'),
+      required: true
+    }
+  }, {
+    key: 'userId',
+    type: 'select',
+    templateOptions: {
+      label: gettextCatalog.getString('User'),
+      options: $scope.plUsers,
+      valueProp: 'id',
+      labelProp: 'name',
+      required: false,
+      disabled: true
+    }
+  }, {
+    key: 'coreItem',
+    type: 'checkbox',
+    templateOptions: {
+      label: gettextCatalog.getString('Core Item?'),
+      required: false
+    }
+  }];
 
 
-    $scope.delete = function (domainItem) {
-        if (domainItem.id) {
-            DomainItemsService.deleteDomainItem(domainItem, function () {
-                $scope.safeDisplayeddomainItems = DomainItemsService.getFilteredDomainItems($scope.currentDomainId);
-                $state.go('^.list');
-            });
-        } else {
-            $state.go('^.list');
-        }
-    };
+  $scope.delete = function (domainItem) {
+    if (domainItem.id) {
+      DomainItemsService.deleteDomainItem(domainItem, function () {
+        $scope.safeDisplayeddomainItems = DomainItemsService.getFilteredDomainItems($scope.currentDomainId);
+        $state.go('^.list');
+      });
+    } else {
+      $state.go('^.list');
+    }
+  };
 
-    $('#fileupload').fileupload({
-        dataType: 'json',
-        done: function (e, data) {
-            //Redisplay domain items
-/*            $.each(data.result.files, function (index, file) {
-                $('<p/>').text(file.name).appendTo(document.body);
-            });*/
-        }
+  $('#fileupload').fileupload({
+    dataType: 'json',
+    submit: function (e, data) {
+      var userId = $scope.currentUser.id.toString();
+      data.formData = {
+        domainId: $scope.currentDomainId,
+        userId: userId
+      };
+      return true;
+    },
+    done: function (e, data) {
+      var gloon = e;
+      //Redisplay domain items
+      /*            $.each(data.result.files, function (index, file) {
+       $('<p/>').text(file.name).appendTo(document.body);
+       });*/
+    }
+  });
+
+  $scope.onSubmit = function () {
+    DomainItemsService.upsertDomainItem($scope.domainItem, function () {
+      $scope.safeDisplayeddomainItems = DomainItemsService.getFilteredDomainItems($scope.currentDomainId);
+      $state.go('^.list');
     });
+  };
 
-    $scope.onSubmit = function () {
-        DomainItemsService.upsertDomainItem($scope.domainItem, function () {
-            $scope.safeDisplayeddomainItems = DomainItemsService.getFilteredDomainItems($scope.currentDomainId);
-            $state.go('^.list');
-        });
-    };
-
-    $scope.loadPicklists = function () {
-        DwDomain.find({filter: {include: []}}).$promise
-            .then(function (allDomains) {
-                $scope.domains.length = 0;
-                for (var i = 0; i < allDomains.length; ++i) {
-                    $scope.domains.push({
-                        value: allDomains[i].name,
-                        name: allDomains[i].name + " - " + allDomains[i].description,
-                        id: allDomains[i].id
-                    });
-                }
-            })
-            .catch(function (err) {
-                console.log(err);
-            })
-            .then(function () {
-                }
-            );
-
-        AminoUser.find({filter: {include: []}}).$promise
-            .then(function (allUsers) {
-                for (var i = 0; i < allUsers.length; ++i) {
-                    $scope.plUsers.push({
-                        value: allUsers[i].username,
-                        name: allUsers[i].firstName + " " + allUsers[i].lastName,
-                        id: allUsers[i].id
-                    });
-                }
-            })
-            .catch(function (err) {
-                console.log(err);
-            })
-            .then(function () {
-                }
-            );
-    };
-
-    $scope.setPageButtons = function (resultLen) {
-        var pageState = '';
-        var fwd = false;
-        var back = false;
-
-        //figure out if we have more than one page of results to see if we enable Fwd
-        if (resultLen >= $scope.itemsPerPage) {
-            fwd = true;
+  $scope.loadPicklists = function () {
+    DwDomain.find({filter: {include: []}}).$promise
+      .then(function (allDomains) {
+        $scope.domains.length = 0;
+        for (var i = 0; i < allDomains.length; ++i) {
+          $scope.domains.push({
+            value: allDomains[i].name,
+            name: allDomains[i].name + " - " + allDomains[i].description,
+            id: allDomains[i].id
+          });
         }
-        //figure out if we're on page greater than page 1 to enable Back
-        if ($scope.itemIndex >= $scope.itemsPerPage) {
-            back = true;
-        }
-        if (fwd && back) {
-            pageState = 'both';
-        } else if (!fwd && back) {
-            pageState = 'backwardOnly';
-        } else if (fwd && !back) {
-            pageState = 'forwardOnly';
-        }
-
-        switch (pageState) {
-            case 'forwardOnly':
-                $('#pageBack').attr('disabled', 'disabled');
-                $('#pageFwd').removeAttr('disabled');
-                break;
-            case 'backwardOnly':
-                $('#pageFwd').attr('disabled', 'disabled');
-                $('#pageBack').removeAttr('disabled');
-                break;
-            case 'both':
-                $('#pageBack').removeAttr('disabled');
-                $('#pageFwd').removeAttr('disabled');
-                break;
-            default: //disabled
-                $('#pageBack').attr('disabled', 'disabled');
-                $('#pageFwd').attr('disabled', 'disabled');
-        }
-    };
-
-    $scope.nextPage = function () {
-        $scope.itemIndex = $scope.itemIndex + $scope.itemsPerPage;
-        $scope.getFilteredPagedResults($scope.currentDomainId, $scope.itemIndex, $scope.itemsPerPage);
-    };
-
-    $scope.prevPage = function () {
-        $scope.itemIndex = $scope.itemIndex - $scope.itemsPerPage;
-        $scope.getFilteredPagedResults($scope.currentDomainId, $scope.itemIndex, $scope.itemsPerPage);
-    };
-
-    $scope.getFilteredPagedResults = function (domainId, itemIndex, itemsPer) {
-        $scope.loading = true;
-
-        DomainItemsService.getFilteredPagedDomainItems(domainId, itemIndex, itemsPer).$promise.then(function (result) {
-            $scope.currentDomainId = domainId;
-            $scope.domainItem = {};
-            $scope.safeDisplayeddomainItems = result;
-            $scope.displayedDomainItems = [].concat($scope.displayedDomainItems);
-
-            $scope.setPageButtons(result.length);
-            $scope.loading = false;
-        });
-    };
-
-    $scope.loading = true;
-    AppAuth.getCurrentUser().then(function (currUser) {
-        $scope.currentUser = currUser;
-        $scope.loadPicklists(currUser);
-        if ($stateParams.id && $stateParams.domainId) {
-            DomainItemsService.getDomainItem($stateParams.id).$promise.then(function (result) {
-                $scope.currentDomainId = $stateParams.domainId;
-                $scope.domainItem = result;
-                $scope.safeDisplayeddomainItems = {};
-                $scope.displayedDomainItems = {};
-                $scope.loading = false;
-            })
-        } else if ($stateParams.domainId) {
-            $scope.getFilteredPagedResults($stateParams.domainId, $scope.itemIndex, $scope.itemsPerPage);
-        } else {
-            DomainItemsService.getDomainItems().$promise.then(function (result) {
-                $scope.currentDomainId = '';
-                $scope.domainItem = {};
-                $scope.safeDisplayeddomainItems = result;
-                $scope.displayedDomainItems = [].concat($scope.displayedDomainItems);
-                $scope.loading = false;
-            });
-        }
-    }, function (err) {
+      })
+      .catch(function (err) {
         console.log(err);
+      })
+      .then(function () {
+        }
+      );
+
+    AminoUser.find({filter: {include: []}}).$promise
+      .then(function (allUsers) {
+        for (var i = 0; i < allUsers.length; ++i) {
+          $scope.plUsers.push({
+            value: allUsers[i].username,
+            name: allUsers[i].firstName + " " + allUsers[i].lastName,
+            id: allUsers[i].id
+          });
+        }
+      })
+      .catch(function (err) {
+        console.log(err);
+      })
+      .then(function () {
+        }
+      );
+  };
+
+  $scope.setPageButtons = function (resultLen) {
+    var pageState = '';
+    var fwd = false;
+    var back = false;
+
+    //figure out if we have more than one page of results to see if we enable Fwd
+    if (resultLen >= $scope.itemsPerPage) {
+      fwd = true;
+    }
+    //figure out if we're on page greater than page 1 to enable Back
+    if ($scope.itemIndex >= $scope.itemsPerPage) {
+      back = true;
+    }
+    if (fwd && back) {
+      pageState = 'both';
+    } else if (!fwd && back) {
+      pageState = 'backwardOnly';
+    } else if (fwd && !back) {
+      pageState = 'forwardOnly';
+    }
+
+    switch (pageState) {
+      case 'forwardOnly':
+        $('#pageBack').attr('disabled', 'disabled');
+        $('#pageFwd').removeAttr('disabled');
+        break;
+      case 'backwardOnly':
+        $('#pageFwd').attr('disabled', 'disabled');
+        $('#pageBack').removeAttr('disabled');
+        break;
+      case 'both':
+        $('#pageBack').removeAttr('disabled');
+        $('#pageFwd').removeAttr('disabled');
+        break;
+      default: //disabled
+        $('#pageBack').attr('disabled', 'disabled');
+        $('#pageFwd').attr('disabled', 'disabled');
+    }
+  };
+
+  $scope.nextPage = function () {
+    $scope.itemIndex = $scope.itemIndex + $scope.itemsPerPage;
+    $scope.getFilteredPagedResults($scope.currentDomainId, $scope.itemIndex, $scope.itemsPerPage);
+  };
+
+  $scope.prevPage = function () {
+    $scope.itemIndex = $scope.itemIndex - $scope.itemsPerPage;
+    $scope.getFilteredPagedResults($scope.currentDomainId, $scope.itemIndex, $scope.itemsPerPage);
+  };
+
+  $scope.getFilteredPagedResults = function (domainId, itemIndex, itemsPer) {
+    $scope.loading = true;
+
+    DomainItemsService.getFilteredPagedDomainItems(domainId, itemIndex, itemsPer).$promise.then(function (result) {
+      $scope.currentDomainId = domainId;
+      $scope.domainItem = {};
+      $scope.safeDisplayeddomainItems = result;
+      $scope.displayedDomainItems = [].concat($scope.displayedDomainItems);
+
+      $scope.setPageButtons(result.length);
+      $scope.loading = false;
     });
+  };
+
+  $scope.loading = true;
+  AppAuth.getCurrentUser().then(function (currUser) {
+    $scope.currentUser = currUser;
+    $scope.loadPicklists(currUser);
+    if ($stateParams.id && $stateParams.domainId) {
+      DomainItemsService.getDomainItem($stateParams.id).$promise.then(function (result) {
+        $scope.currentDomainId = $stateParams.domainId;
+        $scope.domainItem = result;
+        $scope.safeDisplayeddomainItems = {};
+        $scope.displayedDomainItems = {};
+        $scope.loading = false;
+      })
+    } else if ($stateParams.domainId) {
+      $scope.getFilteredPagedResults($stateParams.domainId, $scope.itemIndex, $scope.itemsPerPage);
+    } else {
+      DomainItemsService.getDomainItems().$promise.then(function (result) {
+        $scope.currentDomainId = '';
+        $scope.domainItem = {};
+        $scope.safeDisplayeddomainItems = result;
+        $scope.displayedDomainItems = [].concat($scope.displayedDomainItems);
+        $scope.loading = false;
+      });
+    }
+  }, function (err) {
+    console.log(err);
+  });
 });
 
--- server/boot/file-upload-server.js
+++ server/boot/file-upload-server.js
@@ -1,30 +1,67 @@
+let path = require('path');
+let fs = require('fs');
+let jsonFile = require('jsonfile');
 let options = {
-  tmpDir:  __dirname + '/../public/uploaded/tmp',
+  tmpDir: __dirname + '/../public/uploaded/tmp',
   uploadDir: __dirname + '/../public/uploaded/files',
-  uploadUrl:  '/uploaded/files/',
-  storage : {
-    type : 'local'
+  uploadUrl: '/uploaded/files/',
+  storage: {
+    type: 'local'
   }
 };
 
 let uploader = require('../util/blueimp-file-upload-expressjs/fileupload')(options);
 
-module.exports = function (router) {
-  router.get('/upload', function (req, res) {
+module.exports = function (app) {
+  app.get('/upload', function (req, res) {
     uploader.get(req, res, function (err, obj) {
       res.send(JSON.stringify(obj, 2));
     });
   });
-  router.post('/upload', function (req, res) {
+  app.post('/upload', function (req, res) {
     uploader.post(req, res, function (err, obj) {
+      let fileInfo = obj.files[0];
+      let filePath = path.resolve(fileInfo.options.uploadDir, fileInfo.name);
+      jsonFile.readFile(filePath, (err, domainItemArray) => {
+        fs.unlink(filePath);
+        if (err) {
+          return res.status(200).json(err);
+        }
         //Add domain item to DB
-      res.send(JSON.stringify(obj, 2));
+        app.models.DwDomain.find({where: {id: fileInfo.fields.domainId}}, (err, dwDomains) => {
+          dwDomains[0].domainItems.create({itemValue: domainItemArray[0]}, (err, domainItem)=>{
+            app.models.AminoUser.find({where:{id:fileInfo.fields.userId}},(err,aminoUsers)=>{
+              domainItem.user(aminoUsers[0]);
+              domainItem.save((err,instance)=>{
+                var e = err;
+              });
+            });
+          });
+        });
+        return res.status(200).json(obj);
+      });
     });
   });
-  router.delete('/uploaded/files/:name', function (req, res) {
+  app.delete('/uploaded/files/:name', function (req, res) {
     uploader.delete(req, res, function (err, obj) {
       res.send(JSON.stringify(obj, 2));
     });
   });
-  return router;
+  return app;
 };
+
+function findOrCreateObj(model, query, objToCreate, cb) {
+  try {
+    model.findOrCreate(
+      query,
+      objToCreate, // create
+      function (err, createdObj) {
+        if (err) {
+          log(err);
+        }
+        cb(err, createdObj);
+      });
+  } catch (err) {
+    log(err);
+  }
+}

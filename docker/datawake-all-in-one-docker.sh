7bdac5bb859298e3eac83f0a4a3bf82d6e352088
jreeme <john.reeme@soteradefense.com>
2017-04-04 10:33:22 -0500

    Finish 'centcom' (really DARPA) tweaks for Chris

--- docker/datawake-all-in-one-docker.sh
+++ docker/datawake-all-in-one-docker.sh
@@ -4,4 +4,4 @@
 docker stop datawake
 docker rm datawake
 
-docker run -dt --name datawake -v /tmp:/tmp -h datawake -p 8182:8182 -p 8701:8701 -p 3002:3002 -p 8082:8082 jreeme/datawake:10-MAR-2017
+docker run -dt --name datawake -h datawake -p 8182:8182 -p 8701:8701 -p 3002:3002 -p 8082:8082 jreeme/datawake:dist-04-APR-2017

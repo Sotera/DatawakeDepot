d50cbc74901072eab81ebcc6ae6c3d27d3c46105
nobody.nobody <nobody@nowhere.com>
2017-05-02 10:55:46 -0500

    Update base images to use Node 6

--- docker/datawake-all-in-one-docker.sh
+++ docker/datawake-all-in-one-docker.sh
@@ -4,4 +4,4 @@
 docker stop datawake
 docker rm datawake
 
-docker run -dt --name datawake -h datawake -p 8182:8182 -p 8701:8701 -p 3002:3002 -p 8082:8082 -p 27017:27017 jreeme/datawake:dist-04-APR-2017
+docker run -dt --name datawake -h datawake -p 8182:8182 -p 8701:8701 -p 3002:3002 -p 8082:8082 -p 27017:27017 jreeme/datawake:dist-01-MAY-2017
